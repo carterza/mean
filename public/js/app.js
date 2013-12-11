e00c32c9b31d45c94f62f6709143d537947fbb8f
Zach <carterza@gmail.com>
2013-12-11 00:16:47 -0500

    Setting up initial project

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'underscore', 'mean.system', 'mean.articles']);
 
+angular.module('underscore', []);
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,29 @@
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', '$http', '_', 'Global', function ($scope, $http, _, Global) {
     $scope.global = Global;
+	
+	$scope.breedInfo = undefined;
+	$scope.species = undefined;
+	$scope.breeds = undefined;
+	
+	$scope.selectedSpecies = undefined;
+	$scope.selectedBreed = undefined;
+	
+	$http.jsonp('https://api.rescuegroups.org/http/json?data={"apikey":"sg0aTX1x","objectType":"animalBreeds","objectAction":"publicList","fields":["animalBreeds"]}&callback=JSON_CALLBACK').
+		success(function(data, status) {
+			$scope.breedInfo = data.data;
+			$scope.species = _.chain($scope.breedInfo).pluck('species').unique().value();
+			$scope.breeds = _.chain($scope.breedInfo).pluck('name').unique().value();
+		}).
+		error(function(data, status) {
+			console.log('error');
+			console.log('Data : ' + data);
+			console.log('Status : ' + status);
+		});
+		
+	var mapOptions = {
+		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom: 8
+	};
+	
+	new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
 }]);
